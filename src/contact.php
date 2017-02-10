64a3357a68cd57a16457c2f288485bb529ab677f
Sean Peterson <seanpeterson11@gmail.com>
2017-02-09 18:58:36 -0800

    add finished src/contact.php

--- src/contact.php
+++ src/contact.php
@@ -2,6 +2,49 @@
 
     class contact
     {
+        private $name;
+        private $phone_number;
+        private $address;
+
+        function __construct($name, $phone_number, $address)
+        {
+            $this->name = $name;
+            $this->phone_number = $phone_number;
+            $this->address = $address;
+        }
+
+        function get($property)
+        {
+            if (property_exists($this, $property)) {
+                return $this->$property;
+            } else {
+                return "That is not a property";
+            }
+        }
+
+        function set($property, $value)
+        {
+            if (property_exists($this, $property)){
+                $this[$property] = $value;
+            } else{
+                return "That property does not exist";
+            }
+        }
+
+        function save()
+        {
+            array_push($_SESSION['contact_array'], $this);
+        }
+
+        static function getAll()
+        {
+            return $_SESSION['contact_array'];
+        }
+
+        static function delete()
+        {
+            return $_SESSION['contact_array'] = array();
+        }
 
     }
  ?>
